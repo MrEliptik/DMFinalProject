945b8a90be898e094ad01b55a69c5e712575b3c2
MrEliptik <victormeunier.dev@gmail.com>
2019-01-16 10:04:12 +0200

    added datasets, license, requirements, and update readme

--- README.md
+++ README.md
@@ -1,9 +1,58 @@
 # DMFinalProject
 
+This is a project that uses OPTICS clustering algorithm to cluster footballer faces and diabetic patients' data.
+
 ## Requirements : 
 
 Install scipy==0.21dev0 with : 
 
     pip install git+https://github.com/scikit-learn/scikit-learn.git 
     
-unless OPTICS is now part of the stable release
+unless OPTICS is now part of the stable releas
+
+Install all the rest of the requirements with:
+
+    pip install -r requirements.txt
+
+## Getting Started
+
+### File structure
+
+**Datasets**
+
+    *dataset_diabetes*
+
+        - diabetic_data.csv : 130 US hospital data from 1999 to 2008[1](https://archive.ics.uci.edu/ml/datasets/Diabetes+130-US+hospitals+for+years+1999-2008)
+
+        - IDs_mapping.csv   : mapping for admission type, discharge disposition and admission source
+
+    *footballers*   : 124 footballers' photo (Neymar Jr., Lionel Messi, Cristiano Ronaldo, Luis Suarez, and Mohamed Salah)
+        *Predict*   : 5 footballers' photo to use for prediction (Neymar Jr., Lionel Messi, Cristiano Ronaldo, Luis Suarez, and Mohamed Salah)
+        
+**Ressources**
+
+    - footballers_encodings.pickle          : Encodings of the footballers after using *encode_faces.py*
+
+    - footballers_predict_encodings.pickle  : Encodings of the footballers for prediction after using *encode_faces.py*
+
+    - GUFD_encodings.pickle                 : Encodings of the GUFD photo after using *encode_faces.py*
+
+    - shape_predictor_68_face_landmarks.dat : Used to extract facial features in *encode_features.py*
+
+- encode_faces.py           : used to encode the face in an image as a 128-d vector
+- encode_features.py        : used to encode the facial features of a face as a 7-d vector
+- faces_clustering.py       : used to cluster the footballers' faces
+- diabetic_clustering.py    : used to cluster the diabetic's data
+- similarity_clustering.py  : used to cluster the GUFD dataset based on facial features similiarities
+- optics.py                 : contains the clustering and predicting methods
+
+- requirements.txt          : file containing all the python packages requirements
+
+## Authors
+
+* **Victor MEUNIER** - *DMFinalProject* - [MrEliptik](https://github.com/MrEliptik)
+
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
--- encode_faces.py
+++ encode_faces.py
@@ -5,43 +5,45 @@ import pickle
 import cv2
 import os
 
-dataset_path 	= "Datasets/footballers/"
-encodings_path 	= "Ressources/footballers_encodings.pickle"
 
-# For predictions
-#dataset_path 	= "Datasets/footballers/Predict/"
-#encodings_path 	= "Ressources/footballers_predict_encodings.pickle"
+if __name__ == "__main__":	
+	dataset_path 	= "Datasets/footballers/"
+	encodings_path 	= "Ressources/footballers_encodings.pickle"
 
-# grab the paths to the input images in our dataset, then initialize
-# out data list (which we'll soon populate)
-print(">> Quantifying faces...")
-imagePaths = list(paths.list_images(dataset_path))
-data = []
+	# For predictions
+	#dataset_path 	= "Datasets/footballers/Predict/"
+	#encodings_path 	= "Ressources/footballers_predict_encodings.pickle"
 
-# loop over the image paths
-for (i, imagePath) in enumerate(imagePaths):
-	# load the input image and convert it from RGB (OpenCV ordering)
-	# to dlib ordering (RGB)
-	print(">> Processing image {}/{}".format(i + 1, len(imagePaths)))
-	print(imagePath)
-	image = cv2.imread(imagePath)
-	rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+	# grab the paths to the input images in our dataset, then initialize
+	# out data list (which we'll soon populate)
+	print(">> Quantifying faces...")
+	imagePaths = list(paths.list_images(dataset_path))
+	data = []
 
-	# detect the (x, y)-coordinates of the bounding boxes
-	# corresponding to each face in the input image
-	boxes = face_recognition.face_locations(rgb, model="cnn")
+	# loop over the image paths
+	for (i, imagePath) in enumerate(imagePaths):
+		# load the input image and convert it from RGB (OpenCV ordering)
+		# to dlib ordering (RGB)
+		print(">> Processing image {}/{}".format(i + 1, len(imagePaths)))
+		print(imagePath)
+		image = cv2.imread(imagePath)
+		rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
-	# compute the facial embedding for the face
-	encodings = face_recognition.face_encodings(rgb, boxes)
+		# detect the (x, y)-coordinates of the bounding boxes
+		# corresponding to each face in the input image
+		boxes = face_recognition.face_locations(rgb, model="cnn")
 
-	# build a dictionary of the image path, bounding box location,
-	# and facial encodings for the current image
-	d = [{"imagePath": imagePath, "loc": box, "encoding": enc}
-		for (box, enc) in zip(boxes, encodings)]
-	data.extend(d)
+		# compute the facial embedding for the face
+		encodings = face_recognition.face_encodings(rgb, boxes)
 
-# dump the facial encodings data to disk
-print(">> Serializing encodings...")
-f = open(encodings_path, "wb")
-f.write(pickle.dumps(data))
-f.close()
+		# build a dictionary of the image path, bounding box location,
+		# and facial encodings for the current image
+		d = [{"imagePath": imagePath, "loc": box, "encoding": enc}
+			for (box, enc) in zip(boxes, encodings)]
+		data.extend(d)
+
+	# dump the facial encodings data to disk
+	print(">> Serializing encodings...")
+	f = open(encodings_path, "wb")
+	f.write(pickle.dumps(data))
+	f.close()
